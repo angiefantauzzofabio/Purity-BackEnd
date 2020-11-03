bbd2675da63e421973d2121caf0cdb914a0aee5b
Angelina <usuario@MacBook-Pro-2.local>
2020-11-02 22:00:24 -0300

    Calcula AQI

--- App.js
+++ App.js
@@ -14,13 +14,7 @@ if (!global.atob) { global.atob = decode }
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyA8RZyosskoo3Invm7Iy9QaVo4CLLFR-is",
-    authDomain: "pruebareactnative-c445e.firebaseapp.com",
-    databaseURL: "https://pruebareactnative-c445e.firebaseio.com",
-    projectId: "pruebareactnative-c445e",
-    storageBucket: "pruebareactnative-c445e.appspot.com",
-    messagingSenderId: "660945583741",
-    appId: "1:660945583741:web:20448b91f461646ad87b14"
+   
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
